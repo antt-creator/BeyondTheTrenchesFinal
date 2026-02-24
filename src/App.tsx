7872cd6c47aaeffe999092084cf05e23123e7d25
antt-creator <anttanalytica@gmail.com>
2026-02-24 14:19:16 +0700

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import OrderForm from './components/OrderForm';
-import LoginPage from './LoginPage';
-import AdminDashboard from './AdminDashboard';
-
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* ဝယ်သူတွေ အော်ဒါတင်မယ့် စာမျက်နှာ */}
-        <Route path="/" element={<OrderForm countryCode="TH" onSuccess={() => {}} />} />
-        
-        {/* Admin Login ဝင်မယ့် စာမျက်နှာ */}
-        <Route path="/login" element={<LoginPage />} />
-        
-        {/* Login ဝင်ပြီးမှ ကြည့်လို့ရမယ့် Admin Panel */}
-        <Route path="/admin" element={<AdminDashboard />} />
-      </Routes>
-    </Router>
-  );
-}
 import { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'motion/react';
 import { COUNTRIES, BOOK_INFO, CountryCode } from './constants';
